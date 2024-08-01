6cbb21d22c3907d7c0a957c3fa01c6dfb16a9dd9
prince Umunna <princevictor12@gmail.com>
2024-08-01 13:58:56 +0100

    import Home, Header and Cart component. Mount the header component inside the BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Header from './components/Header';
+import Cart from './components/Cart';
 function App() {
   return (
      <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/cart' element={<Cart />} />
