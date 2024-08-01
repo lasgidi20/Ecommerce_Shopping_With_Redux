4f48050935f16034a4d9daecf54cb64d8f725ff4
prince Umunna <princevictor12@gmail.com>
2024-08-01 21:45:05 +0100

    import the useSelector and useDispatch hook. Map through the items accessed with the useSelector Hook and pass an item as a prop to a SingleProduct component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,28 @@ import React, {useEffect, memo} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { FetchProductData, AddToCart, RemoveFromCart, ClearCart } from '../Slice/ProductSlice'
 import { fetchProducts } from '../APIs/fetchProducts';
+import SingleProduct from './SingleProduct';
+import Filter from './Filter';
+import './style.css'
 
 const Home = () => {
-    const items = useSelector(state => state.prod_cart_management.products)
+    const items = useSelector(state => state.prod_cart_management.products.products)
     console.log(items)
+    console.log(Array.isArray(items))
     const dispatch = useDispatch()
     useEffect(() => {
         dispatch(fetchProducts())
     }, [])
-    
   return (
-    <>
-     
-    </>
+    <div className='home'>
+        <Filter />
+       <div className='prod-container'>
+         {items.length > 0 ? (items.map((item) => {
+          return  <SingleProduct item={item} />
+         })) : ("no products")}
+       </div>
+    </div>
   )
 }
 
-export default memo(Home)
+export default Home
