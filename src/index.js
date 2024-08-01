c38b302f34c8cc4de8928d7954811b0653f90afb
prince Umunna <princevictor12@gmail.com>
2024-08-01 14:24:20 +0100

    import the store and Provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { store } from './Store';
+import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
