e92b62be08afd1f8e744a7df428ff668e6e1b232
prince Umunna <princevictor12@gmail.com>
2024-08-01 16:53:39 +0100

    import the useSelector and useDispatch hooks, useSelector hook allows us to access to the store, useDispatch hook allows us dispatch action types from the the reducer in our components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
+import React, {useEffect, memo} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { FetchProductData, AddToCart, RemoveFromCart, ClearCart } from '../Slice/ProductSlice'
+import { fetchProducts } from '../APIs/fetchProducts';
+import { useStore } from "react-redux";
 const Home = () => {
+    const items = useSelector(state => state.prod_cart_management.products)
+    console.log(items)
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, [])
+    
   return (
-    <div>Home</div>
+    <>
+
+    </>
   )
 }
 
-export default Home
+export default memo(Home)
