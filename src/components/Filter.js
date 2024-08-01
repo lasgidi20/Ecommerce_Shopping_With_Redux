a384cc2d7c1c6a6e220f23f2f059a140da180392
prince Umunna <princevictor12@gmail.com>
2024-08-01 22:19:40 +0100

    filter component that will be responsible for filtering of products by price, shipping details and warranty information. Form.check is used which is imported from bootstrap

---
+++ src/components/Filter.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form';
+const Filter = () => {
+  return (
+    <div className='filters'>
+       <span className='title'>Filter By Category</span>
+       <span>
+         <Form.Check
+            inline
+            label="Sort Price Ascending"
+            name="group1"
+            type="radio"
+          />
+       </span>
+       <span>
+         <Form.Check
+             inline
+             label="Sort Price Descending"
+             name="group1"
+             type="radio"
+          />
+        </span>
+        <span>
+        <Form.Check
+          label="Shipping Details"
+          name="group2"
+          type="checkbox"
+        />
+        </span>
+        <span>
+          <Form.Check
+            inline
+            label="Warranty Information"
+            name="group2"
+            type="checkbox"
+          />
+        </span>
+        <span>
+            <label style={{paddingRight: "11px"}}>Rating:</label>
+        </span>
+        <Button variant="light">Clear Filters</Button>
+    </div>
+  )
+}
+
+export default Filter
