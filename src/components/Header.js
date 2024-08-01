498775a5b3061790ceb4d63465538f7f783aa1d1
prince Umunna <princevictor12@gmail.com>
2024-08-01 13:54:02 +0100

    added react-bootstrap form control and the Dropdown component. React icons is also imported to use the shopping cart image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import { Navbar } from 'react-bootstrap'
+import { FaCartShopping } from "react-icons/fa6";
 const Header = () => {
   return (
     <Navbar bg='dark' variant='dark' style={{height: 80}}>
     <Container>
       <Navbar.Brand href="#home">Shopping Cart</Navbar.Brand>
+      <Form.Control
+        type="text"
+        placeholder="Search an Item"
+        style={{width: 500}}
+        className="m-auto"
+      />
+    <Dropdown align="end">
+      <Dropdown.Toggle variant="success">
+        <FaCartShopping /> <span style={{paddingRight: "6px"}}>{0}</span>
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu style={{width: "370px"}}>
+         <span style={{padding: "11px"}}>Cart is Empty</span>
+      </Dropdown.Menu>
+    </Dropdown>
     </Container>
     </Navbar>
   )
