c902c356881fdb1d3379bb8551fc09145643fdb4
prince Umunna <princevictor12@gmail.com>
2024-08-02 07:27:04 +0100

    Use the UseSelector hook to obtain the items stored in the cart and map through it to display it in the dropdown for items in the cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap'
+import { Button, Navbar } from 'react-bootstrap'
 import { FaCartShopping } from "react-icons/fa6";
 import Container from 'react-bootstrap/Container';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Form from 'react-bootstrap/Form';
 import { useSelector, useDispatch } from 'react-redux'
+import { AiFillDelete } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import './style.css';
 
 const Header = () => {
     const items = useSelector(state => state.prod_cart_management.cart)
@@ -25,7 +28,25 @@ const Header = () => {
       </Dropdown.Toggle>
 
       <Dropdown.Menu style={{width: "370px"}}>
-         <span style={{padding: "11px"}}>Cart is Empty</span>
+         {items.length > 0 ? items.map((item) => (<span className="cartItems">
+            <img src={item.thumbnail} 
+                alt={item.title}
+                className='itemImg'
+            />
+
+            <div className='cart-detail'>
+                <span>{item.title}</span>
+                <span>{item.price}</span>
+            </div>
+
+            <AiFillDelete
+            fontSize="20px"
+            />
+
+         </span>)) : (<span style={{padding: "11px"}}>Cart is Empty</span>)}
+         <Link to='/'>
+         <Button style={{width: "95%", marginLeft: "10px", marginTop: "10px"}}>Cart Page</Button>
+         </Link>
       </Dropdown.Menu>
     </Dropdown>
     </Container>
