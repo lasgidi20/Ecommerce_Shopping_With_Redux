b41c06a89d717cc7549e73db862ce165c9696ff4
prince Umunna <princevictor12@gmail.com>
2024-08-02 17:38:41 +0100

    Updated the setTotal callback action. I have also added an Onchange event with  a dispatch to IncreaseQty action and passed a payload with its id and quantity which is the event.target.value

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,18 @@
 import React, {useState, useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { ListGroup } from 'react-bootstrap'
+import { Button, ListGroup } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
 import { Form } from 'react-bootstrap';
+import { ClearCart, IncreaseQty } from '../Slice/ProductSlice';
 const Cart = () => {
     const items = useSelector(state => state.prod_cart_management.cart)
     console.log(items)
     const dispatch = useDispatch()
     const [total, setTotal] = useState(0)
     useEffect(() => {
-        setTotal(items.reduce((acc, curr) => acc + Number(curr.price), total))
+        setTotal(items.reduce((acc, curr) => acc + Number(curr.price) * curr.qty, 0))
     }, [items])
 
   return (
@@ -25,9 +26,15 @@ const Cart = () => {
                   <Col md={2}><span>{item.title}</span></Col>
                   <Col md={2}><span>£{item.price}</span></Col>
                   <Col md={2}><span>{item.rating}</span></Col>
-                  <Col md={2}> <Form.Control as='select' value={item.qty}>
+                  <Col md={2}> <Form.Control as="select" value={item.qty} onChange={(e) => dispatch({
+                     type: IncreaseQty,
+                     payload: {
+                        id: item.id,
+                        qty: e.target.value
+                     },
+                  })}>
                       {[...Array(item.stock).keys()].map((x) => (
-                      <option value="x + 1">{x + 1}</option>
+                      <option key={x + 1}>{x + 1}</option>
                       ))}</Form.Control></Col>
                 </Row>
                 </ListGroup.Item>
@@ -37,6 +44,7 @@ const Cart = () => {
         <div className='filters'>
            <span> Subtotal: {items.length} Items In Cart</span>
            <span style={{fontSize: "30px", fontWeight: 500}}>Total: £{total}</span>
+           <Button>Pay</Button>
         </div>
     </div>
   )
