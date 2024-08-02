3c4e1984106235a4bb1f9b77fe7f936f814be667
prince Umunna <princevictor12@gmail.com>
2024-08-02 13:54:03 +0100

    Add the useState hook for getting the total of a cart. The useEffect hooks takes in the setTotal call back function which uses javascript reduce method to get the toal of items. the items in the basket are being monitored by the  items array in the useEffect hook.

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { ListGroup } from 'react-bootstrap'
 import Row from 'react-bootstrap/Row';
@@ -9,6 +9,11 @@ const Cart = () => {
     const items = useSelector(state => state.prod_cart_management.cart)
     console.log(items)
     const dispatch = useDispatch()
+    const [total, setTotal] = useState(0)
+    useEffect(() => {
+        setTotal(items.reduce((acc, curr) => acc + Number(curr.price), total))
+    }, [items])
+
   return (
     <div className='home'>
         <div className='prod-container'>
@@ -18,14 +23,12 @@ const Cart = () => {
                 <Row>
                   <Col md={2}> <Image variant='top' src={item.images[0]} rounded fluid/></Col>
                   <Col md={2}><span>{item.title}</span></Col>
-                  <Col md={2}><span>{item.price}</span></Col>
+                  <Col md={2}><span>£{item.price}</span></Col>
                   <Col md={2}><span>{item.rating}</span></Col>
-                  <Col md={2}>    <Form.Control as='select' value={item.qty}>
-                                      {[...Array(item.stock).keys()].map((x) => (
-                                         <option value="x + 1">{x + 1}</option>
-                                      ))}
-                                  </Form.Control></Col>
-                
+                  <Col md={2}> <Form.Control as='select' value={item.qty}>
+                      {[...Array(item.stock).keys()].map((x) => (
+                      <option value="x + 1">{x + 1}</option>
+                      ))}</Form.Control></Col>
                 </Row>
                 </ListGroup.Item>
              </ListGroup>
@@ -33,7 +36,7 @@ const Cart = () => {
         </div>
         <div className='filters'>
            <span> Subtotal: {items.length} Items In Cart</span>
-           <span style={{fontSize: "30px", fontWeight: 500}}>Total: {'total'}</span>
+           <span style={{fontSize: "30px", fontWeight: 500}}>Total: £{total}</span>
         </div>
     </div>
   )
