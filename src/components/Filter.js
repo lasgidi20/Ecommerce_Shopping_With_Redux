061e6f7452312430d9b3c28867346cf95f8d5ed6
prince Umunna <princevictor12@gmail.com>
2024-08-03 14:08:42 +0100

    Update the filter categories names in the form, including more actions to dispatch which is imported from ProductFilterSlice which will help filters for products with a months warranty and year warranty. There is also a clearFilter action that is dispatched which resets the whole filtering to its default

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -3,7 +3,7 @@ import { Button } from 'react-bootstrap'
 import Form from 'react-bootstrap/Form';
 import Rating from './Rating';
 import {useDispatch} from 'react-redux'
-import {SortPriceAscending, SortPriceDescending, ShipOvernight} from '../Slice/ProductSlice'
+import {SortPriceAscending, SortPriceDescending, ShipOvernight, Ship2week, Ship1_2days, MonthsWarranty, YearWarranty, ClearFilter, Ship1_Month} from '../Slice/ProductFilterSlice'
 const Filter = () => {
   const [rate, setRate] = useState(3)
   const dispatch = useDispatch();
@@ -34,7 +34,7 @@ const Filter = () => {
         </span>
         <span>
         <Form.Check
-          label="Shipping Details"
+          label="Shipped OverNight"
           name="group2"
           type="checkbox"
           onClick={() => dispatch({
@@ -45,18 +45,68 @@ const Filter = () => {
         <span>
           <Form.Check
             inline
-            label="Warranty Information"
+            label="Shipped In 2 Weeks"
             name="group2"
             type="checkbox"
-            onClick={() => dispatch({
-              type: ShipOvernight
+            onChange={() => dispatch({
+              type: Ship2week
+            })}
+          />
+        </span>
+
+        <span>
+          <Form.Check
+            inline
+            label="Shipped In 1-2 Days"
+            name="group2"
+            type="checkbox"
+            onChange={() => dispatch({
+              type: Ship1_2days
+            })}
+          />
+        </span>
+
+        <span>
+          <Form.Check
+            inline
+            label="Ships in 1 month"
+            name="group2"
+            type="checkbox"
+            onChange={() => dispatch({
+              type: Ship1_Month
+            })}
+          />
+        </span>
+
+        <span>
+          <Form.Check
+            inline
+            label="Products with A Month Warranty"
+            name="group2"
+            type="checkbox"
+            onChange={() => dispatch({
+              type:  MonthsWarranty
+            })}
+          />
+        </span>
+
+        <span>
+          <Form.Check
+            inline
+            label="Products with A Year Warranty"
+            name="group2"
+            type="checkbox"
+            onChange={() => dispatch({
+              type: YearWarranty
             })}
           />
         </span>
         <span>
-            <label style={{paddingRight: "11px"}}> <Rating rate={rate} style={{cursor: "pointer"}} onClick={(i) => setRate(i)}/></label>
+            <label style={{paddingRight: "11px"}}> Sort By Rating: <Rating rate={rate} style={{cursor: "pointer"}} onClick={(i) => setRate(i)}/></label>
         </span>
-        <Button variant="light">Clear Filters</Button>
+        <Button variant="light" onClick={() => dispatch({
+           type: ClearFilter
+        })}>Clear Filters</Button>
     </div>
   )
 }
