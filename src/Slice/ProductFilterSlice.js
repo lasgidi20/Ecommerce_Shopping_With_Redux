262fcf8d2bce3cb4bf2543f3be147d3916e63f36
prince Umunna <princevictor12@gmail.com>
2024-08-03 14:11:38 +0100

    Product filter slice responsible for filtering products by various actions in the reducer

---
+++ src/Slice/ProductFilterSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { fetchProducts } from '../APIs/fetchProducts'
+
+const initialState = {
+  products: [],
+  sort:'',
+  shipping: '',
+  warranty: ''
+}
+
+export const productFilterSlice = createSlice({
+  name: 'product_filter',
+  initialState,
+  reducers: {
+    FetchProductData: (state, action) => {
+      return {...state, products: action.payload}
+    },
+
+    SortPriceAscending: (state) => {
+      state.sort = state.products.sort((a,b) => a.price - b.price )
+    },
+
+    SortPriceDescending: (state) => {
+      state.sort =  state.products.sort((a,b) =>  b.price - a.price )
+    },
+
+    ShipOvernight: (state) => {
+       state.shipping = state.products.filter((item) => item.shippingInformation === 'Ships overnight' )
+    },
+
+    Ship2week: (state) => {
+      state.shipping = state.products.filter((item) => item.shippingInformation === 'Ships in 2 weeks' )
+    },
+
+   Ship1_2days: (state) => {
+      state.shipping = state.products.filter((item) => item.shippingInformation === 'Ships in 1-2 business days' )
+   },
+
+   Ship1_Month: (state) => {
+      state.shipping = state.products.filter((item) => item.shippingInformation === 'Ships in 1 month' )
+   },
+
+    MonthsWarranty: (state) => {
+       state.warranty = state.products.filter((item) => item.warrantyInformation === '1 month warranty' )
+    },
+    YearWarranty: (state) => {
+       state.warranty = state.products.filter((item) => item.warrantyInformation === '1 year warranty' )
+    },
+    ClearFilter: (state) => {
+       return {...state, sort: '', shipping: '', warranty: ''}
+    },
+  },
+})
+
+export const { FetchProductData, SortPriceAscending, SortPriceDescending, ShipOvernight, Ship1_2days, Ship2week, Ship1_Month, MonthsWarranty, YearWarranty, ClearFilter } = productFilterSlice.actions
+
+export default productFilterSlice.reducer
