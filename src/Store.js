2da467c33624ad69d9c379ce1d52bea03048c9de
prince Umunna <princevictor12@gmail.com>
2024-08-03 14:14:19 +0100

    ProductFilterSlice is imported to the store which will make it avilable to access from the store

--- src/Store.js
+++ src/Store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import productSlice from './Slice/ProductSlice'
+import productFilterSlice from './Slice/ProductFilterSlice'
 export const store = configureStore({
   reducer: {
-    prod_cart_management: productSlice
+    prod_cart_management: productSlice,
+    product_filter: productFilterSlice
   },
 })
