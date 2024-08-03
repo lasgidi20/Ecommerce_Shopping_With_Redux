2106a87def5586d3b49f3e20dea59f67bbad2576
prince Umunna <princevictor12@gmail.com>
2024-08-03 10:29:54 +0100

    import the action FetchProductData from our Productslice and then dispatch the data obtained from the api call to the reducer

--- src/APIs/fetchProducts.js
+++ src/APIs/fetchProducts.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { FetchProductData } from '../Slice/ProductSlice'
 export const fetchProducts = () => dispatch => {
+    console.log('dispatching')
     fetch("https://dummyjson.com/products", {
         headers: {
              "Content-Type": "application/json",
@@ -10,6 +11,6 @@ export const fetchProducts = () => dispatch => {
     .then(res => res.json())
     .then(data => dispatch({
         type: FetchProductData,
-        payload: data
+        payload: data.products
     }))
 }
