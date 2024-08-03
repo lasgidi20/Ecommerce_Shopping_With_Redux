c69d1bce721c4a31d2130459294a71032f0326ea
prince Umunna <princevictor12@gmail.com>
2024-08-03 14:21:14 +0100

    I am accessing the shipping information from the store here and also accessing the warranty from the store with the useSelector Hook. In regards to filtering by shipping or warranty information, there is a conditional that sets the items to display based on if filtering for shipping is checked or warranty is checked

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,27 @@
 import React, {useEffect, memo} from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { FetchProductData, AddToCart, RemoveFromCart, ClearCart } from '../Slice/ProductSlice'
+import { FetchProductData, Ship1_2days, ShipOvernight, Ship2week, MonthsWarranty, YearWarranty, ClearFilter } from '../Slice/ProductFilterSlice';
 import { fetchProducts } from '../APIs/fetchProducts';
 import SingleProduct from './SingleProduct';
 import Filter from './Filter';
 import './style.css'
-import { fetchItems } from '../APIs/fetchProducts';
 
 const Home = () => {
-   console.log(fetchItems())
-    const items = useSelector(state => state.prod_cart_management.products)
+    let items = useSelector(state => state.product_filter.products)
+    let shipping_information =  useSelector(state => state.product_filter.shipping)
+    let warranty_information =  useSelector(state => state.product_filter.warranty)
+    console.log(shipping_information)
+
     const dispatch = useDispatch()
     useEffect(() => {
       dispatch(fetchProducts())
     },[])
+
+    if (shipping_information) {
+      items = shipping_information
+    } else if(warranty_information){
+      items = warranty_information
+    }
   return (
     <div className='home'>
         <Filter />
